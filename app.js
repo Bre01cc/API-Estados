8bf3f0ddca7dae9880447cdcc143f9798f8cbf2c
BRENO OLIVEIRA ASSIS REIS <25133656@cfp127aluno.local>
2025-09-17 11:25:51 -0300

    Testando requisições

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
  *  npm install body-parser --save
  ****************************************************************************************************************************
  */
-
+const dados = require('./modulo/funcoes.js')
 const express = require('express')//Responsável pela API
 const cors = require('cors')//Responsável pela permissão de API(APP)
 const bodyParser = require('body-parser')//Responsável por gerenciar a chegada dos dados da API com o front
@@ -22,16 +22,56 @@ const app = express()
 
 //Configurações de permissões
 app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin','*')//Servidor de origem da API
-    response.header('Access-Control-Allow-Methods','GET')//Verbos permitidos na API
+    response.header('Access-Control-Allow-Origin', '*')//Servidor de origem da API
+    response.header('Access-Control-Allow-Methods', 'GET')//Verbos permitidos na API
     //Carregar as configurações no CORS da API
     app.use(cors())
     //Proximo, carregar os proximos endpoints
     next()
 })
 
+//Request -> chegada de dados na API
+//Response -> Retorno de dados na API
+
 //ENDPOINTS
-app.get('/v1/estados',(res,req)[
-    
-])
+app.get('/v1/estados', function (request, response) {
+    let estados = dados.getAllEstados()
+    //Retorna o status code
+    response.status(estados.status_code)
+    //Retorna o JSON
+    response.json(estados)
+}
+)
+app.get('/v1/estados/:uf',function (request, response) {
+    let sigla = request.params.uf
+    let infoEstado = dados.getAllEstadoBySigla(sigla)
+    response.status(infoEstado.status_code)
+    //Retorna o JSON
+    response.json(infoEstado)
+})
+app.get('/v1/capital/:uf',function (request, response) {
+    let sigla = request.params.uf
+    let infoCapital = dados.getCapitalBySigla(sigla)
+    response.status(infoCapital.status_code)
+    //Retorna o JSON
+    response.json(infoCapital)
+})
+app.get('/v1/regiao/:uf',function (request, response) {
+    let regiao = request.params.uf
+    let infoRegiao = dados.getEstadosByRegiao(regiao)
+    response.status(infoRegiao.status_code)
+    //Retorna o JSON
+    response.json(infoRegiao)
+})
+app.get('/v1/pais/capitais/',function (request, response) {
+    let infoPaisCapital = dados.getVerifyCapitaisDoPais()
+    response.status(infoPaisCapital.status_code)
+    //Retorna o JSON
+    response.json(infoPaisCapital)
+})
+
+//Start na API
+app.listen(PORT, function () {
+    console.log('API aguardando requisições.....')
+})
 
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -7,6 +7,7 @@
  */
 
 
+ //Criando um array de mensagens
 const MENSAGE_ERRO = [
 
     {
@@ -26,32 +27,25 @@ const MENSAGE_ERRO = [
     }
 
 ]
-console.log(MENSAGE_ERRO[0])
-
-
-
-// const MENSAGE_ERROR = {
-//     status: false,
-//     status_code: 500,
-//     development: "Breno Oliveira Assis Reis"
-// }
-//Por boas práticas colocamos no arquivo ".js"
 const dados = require('./estado_cidades.js')
 
 //Retorna todos os estados
 const getAllEstados = () => {
 
-    //(item) é o nome de referencia do retorno do forEach
+    //Variavel de base para o cabeçalho
     let mensage = {
         status: true, status_code: 200, development: "Breno Oliveira Assis Reis", uf: [
 
         ]
     }
+    //(item) é o nome de referencia do retorno do forEach
+    //Loop, forEach apenas para array, percorre todos os index do array e devolve o que estiver lá dentro
     dados.listaDeEstados.estados.forEach((item) => {
         mensage.uf.push(item.sigla)
     })
+    //Para adicionar um elemento novo no JSON
     mensage.quantidade = mensage.uf.length
-
+    //delete mensage(para remover um atributo de um JSON)
     if (mensage.uf.length > 0)
         return mensage //Verdadeira status 200
     else
@@ -67,10 +61,8 @@ const getAllEstadoBySigla = (sigla) => {
 
     let indexEstado = dados.listaDeEstados.estados.findIndex(estados => estados.sigla === siglaEstado)
 
-    let quantidade = dados.listaDeEstados.estados.length
-    if (indexEstado < quantidade) {
+    if (indexEstado == -1)
         return MENSAGE_ERRO[2]
-    }
     else {
         let estado = dados.listaDeEstados.estados[indexEstado]
 
@@ -81,28 +73,109 @@ const getAllEstadoBySigla = (sigla) => {
         return mensage
     }
 }
-console.log(getAllEstadoBySigla('sr'))
 
 //Retorna a capital referente a um estado pela sua determinada sigla.
 const getCapitalBySigla = (sigla) => {
+    if (!sigla)
+        return MENSAGE_ERRO[2]
+    let siglaEstado = sigla.toLocaleUpperCase()
+
+    let indexEstado = dados.listaDeEstados.estados.findIndex(estados => estados.sigla === siglaEstado)
+
+    if (indexEstado == -1)
+        return MENSAGE_ERRO[1]
+    else {
+        let estado = dados.listaDeEstados.estados[indexEstado]
+
+        let mensage = {
+            status: true, status_code: 200, development: "Breno Oliveira Assis Reis", uf: estado.sigla,
+            descricao: estado.nome, capital: estado.capital
+        }
+        return mensage
+    }
 
 }
+
+
 //Retorna uma lista de estados pesquisando pela região.
 const getEstadosByRegiao = (regiao) => {
+    let mensage = {
+        status: true, status_code: 200, development: "Breno Oliveira Assis Reis", regiao: regiao,
+        estados: []
 
+    }
+    dados.listaDeEstados.estados.forEach((item) => {
+        if (item.regiao == regiao) {
+            //Adicionando objetos no Json estados
+            //E a cada item.(atributo) ele será adicionado junto a uf:''
+            //ou descricao:''
+            mensage.estados.push({
+                uf: item.sigla,
+                descricao: item.nome
+            })
+        }
+    })
+
+
+    return mensage
 }
+
 //Retorna uma lista de estados referente as capitais do país.
 const getVerifyCapitaisDoPais = () => {
+    let mensage = {
+        status: true, status_code: 200, development: "Breno Oliveira Assis Reis", capitais: [
 
+        ]
+    }
+    //(item) é o nome de referencia do retorno do forEach
+    //Loop, forEach apenas para array, percorre todos os index do array e devolve o que estiver lá dentro
+    dados.listaDeEstados.estados.forEach((item) => {
+        if (item.capital_pais) {
+            mensage.capitais.push({
+                capital_pais: item.capital_pais.capital,
+                uf: item.sigla,
+                regiao: item.regiao,
+                capital_pais_ano_inicio: item.capital_pais.ano_inicio,
+                capital_pais_ano_termino: item.capital_pais.ano_fim
+            })
+        }
+
+
+    })
+    return mensage
 }
-//Retorna uma lista de cidades pesquisando pela sigla do estado.
+//Para adicionar um elemento novo no JSON  
+getVerifyCapitaisDoPais()
+
 const getCidadesBySigla = (sigla) => {
+    if (!sigla)
+        return MENSAGE_ERRO[1]
+    let siglaEstado = sigla.toLocaleUpperCase()
 
+    let indexEstado = dados.listaDeEstados.estados.findIndex(estados => estados.sigla === siglaEstado)
+
+    if (indexEstado == -1)
+        return MENSAGE_ERRO[2]
+    else {
+        let estado = dados.listaDeEstados.estados[indexEstado]
+        let cidade = estado.cidades
+
+        let mensage = {
+            status: true, status_code: 200, development: "Breno Oliveira Assis Reis", uf: estado.sigla,
+            descricao: estado.nome, quantidade: cidade.length, cidades: []
+        }
+        cidade.forEach((item) => {
+
+            mensage.cidades.push(item.nome)
+        })
+        return mensage
+    }
 }
-// if(item.regiao=='Norte'){
-//     console.log(item.sigla,item.regiao)
-// }
-
 module.exports = {
-    getAllEstados
+    getAllEstados,
+    getAllEstadoBySigla,
+    getCidadesBySigla,
+    getVerifyCapitaisDoPais,
+    getCapitalBySigla,
+    getEstadosByRegiao
 }
